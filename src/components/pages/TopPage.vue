cf82e4737e74f311bdbec0f5bf2abcf9363f46a9
Uhucream <aborog1324@yahoo.co.jp>
2021-01-27 19:31:06 +0900

    写真を変更

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -17,7 +17,10 @@
     <v-main>
       <v-carousel
         hide-delimiter-background
+        hide-delimiters
         show-arrows-on-hover
+        cycle
+        height="400px"
       >
         <v-carousel-item
           v-for="(item,i) in carouselItems"
@@ -38,8 +41,16 @@
               </v-card>
             </v-col>
           </v-row>
+          <v-divider></v-divider>
+          <v-subheader>My works</v-subheader>
+          <v-row>
+            <v-col>
+              <v-card>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-divider></v-divider>
         </v-list>
-        <v-divider></v-divider>
       </v-container>
     </v-main>
   </v-app>
@@ -52,16 +63,16 @@ export default {
     return {
       carouselItems: [
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+          src: 'https://lh3.googleusercontent.com/pw/ACtC-3dethR4ungoJiKTTxDZMx3IPMCFgBu-epL066EznDpmKV4JV1KZSRHpByBTMfj5e_hD8YpTvBSwLyWZs7i2qJ_jzgBwIx_uP1byuaw-U8Vl4An5tI7qywpvuhQquQvcAX30zVR0gLyP5Hdp3MyrkCtvNQ=s0'
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+          src: 'https://lh3.googleusercontent.com/pw/ACtC-3eCM_yJQ6-bvBX9IGgAmbzD4dMDaCDybDH47hxUH9l6zfo3zwzrZxj7dLNLilV2MqSkGq9yFZTJvDUDToMoWMVnFvmB0RDHEM2iL1GdwcO7kOTJp5tzvO9ljq2H0h-erhnnfn5ZxkA3xZ9VtMMOFQWrew=s0'
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+          src: 'https://lh3.googleusercontent.com/pw/ACtC-3chRUQ6r2UAnU3xVXYmB1ySfG6pSJC_DeXil7ykFSBleq0fP6_uc2pNFeK11VyBo2yfp5D5t8jftqP2JzQ_vqE7hMqgS07Whj_RXgikEli_9m48fwN3KeEoXuekyRO8RhrDsixPWJeJaxe7_dL0Kny_lw=s0'
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+          src: 'https://lh3.googleusercontent.com/pw/ACtC-3d0cMieyC5qvR9JTFUXH8UkeRpoTwAexxp74TGaige2Rg-S1WL8dDoXms798DUiprXfdJf-r-cxEf4hJE4FAt6mcEI7z7rdehuNMrPfa03ttPW8x_B7gG1SmhO8cid_BweFApns43YzCK64chg-Lvyt9g=s0'
         }
       ]
     }
