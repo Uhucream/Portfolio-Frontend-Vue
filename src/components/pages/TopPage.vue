f895f22ae4b101485d27d69a49be63972897d10d
Uhucream <aborog1324@yahoo.co.jp>
2021-01-27 18:23:50 +0900

    アイコンが表示されない問題修正&name修正

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -15,13 +15,31 @@
     </v-app-bar>
 
     <v-main>
+      <v-carousel
+        hide-delimiter-background
+        show-arrows-on-hover
+      >
+        <v-carousel-item
+          v-for="(item,i) in carouselItems"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        ></v-carousel-item>
+      </v-carousel>
       <v-container fluid>
-        <v-row dense>
-          <v-col>
-            <v-card>
-            </v-card>
-          </v-col>
-        </v-row>
+        <v-subheader>About Me</v-subheader>
+        <v-list
+          flat
+        >
+          <v-row>
+            <v-col>
+              <v-card>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-list>
+        <v-divider></v-divider>
       </v-container>
     </v-main>
   </v-app>
@@ -29,6 +47,24 @@
 
 <script>
 export default {
-
+  name: 'topPage',
+  data () {
+    return {
+      carouselItems: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>>
