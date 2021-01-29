7a1a2792efe3df09ba4b6bef2406e5dd16fcfb4e
Uhucream <aborog1324@yahoo.co.jp>
2021-01-29 18:02:52 +0900

    レイアウト整理

--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -67,72 +67,157 @@
         ></v-carousel-item>
       </v-carousel>
       <v-container fluid>
-        <v-list
-          flat
-          class="tile"
-        >
-          <v-subheader>About Me</v-subheader>
-          <v-row>
-            <v-col>
-              <v-card>
-                <v-card-text>
-                  <div>Word of the Day</div>
-                  <p class="display-1 text--primary">
-                    el·ee·mos·y·nar·y
-                  </p>
-                  <p>adjective</p>
-                  <div class="text--primary">
-                    relating to or dependent on charity; charitable.<br>
-                    "an eleemosynary educational institution."
-                  </div>
-                </v-card-text>
-                <v-card-actions>
-                  <v-btn
-                    text
-                    color="teal accent-4"
-                    @click="reveal = true"
-                  >
-                    Learn More
-                  </v-btn>
-                </v-card-actions>
+        <v-row>
+          <v-col>
+            <v-subheader>About Me</v-subheader>
+            <v-card>
+              <v-card-text>
+                <div>Word of the Day</div>
+                <p class="display-1 text--primary">
+                  el·ee·mos·y·nar·y
+                </p>
+                <p>adjective</p>
+                <div class="text--primary">
+                  relating to or dependent on charity; charitable.<br>
+                  "an eleemosynary educational institution."
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="teal accent-4"
+                  @click="reveal = true"
+                >
+                  Learn More
+                </v-btn>
+              </v-card-actions>
 
-                <v-expand-transition>
-                  <v-card
-                    v-if="reveal"
-                    class="transition-fast-in-fast-out v-card--reveal"
-                    style="height: 100%;"
-                  >
-                    <v-card-text class="pb-0">
-                      <p class="display-1 text--primary">
-                        Origin
-                      </p>
-                      <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
-                    </v-card-text>
-                    <v-card-actions class="pt-0">
-                      <v-btn
-                        text
-                        color="teal accent-4"
-                        @click="reveal = false"
-                      >
-                        Close
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-expand-transition>
+              <v-expand-transition>
+                <v-card
+                  v-if="reveal"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%;"
+                >
+                  <v-card-text class="pb-0">
+                    <p class="display-1 text--primary">
+                      Origin
+                    </p>
+                    <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+                  </v-card-text>
+                  <v-card-actions class="pt-0">
+                    <v-btn
+                      text
+                      color="teal accent-4"
+                      @click="reveal = false"
+                    >
+                      Close
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-subheader>My Works</v-subheader>
+            <v-card>
+              <v-card-text>
+                <div>Word of the Day</div>
+                <p class="display-1 text--primary">
+                  el·ee·mos·y·nar·y
+                </p>
+                <p>adjective</p>
+                <div class="text--primary">
+                  relating to or dependent on charity; charitable.<br>
+                  "an eleemosynary educational institution."
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="teal accent-4"
+                  @click="reveal = true"
+                >
+                  Learn More
+                </v-btn>
+              </v-card-actions>
 
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-          <v-subheader>My works</v-subheader>
-          <v-row>
-            <v-col>
-              <v-card>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-        </v-list>
+              <v-expand-transition>
+                <v-card
+                  v-if="reveal"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%;"
+                >
+                  <v-card-text class="pb-0">
+                    <p class="display-1 text--primary">
+                      Origin
+                    </p>
+                    <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+                  </v-card-text>
+                  <v-card-actions class="pt-0">
+                    <v-btn
+                      text
+                      color="teal accent-4"
+                      @click="reveal = false"
+                    >
+                      Close
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+          </v-col>
+          <v-col
+            cols="12"
+          >
+            <v-subheader>My Works</v-subheader>
+            <v-card>
+              <v-card-text>
+                <div>Word of the Day</div>
+                <p class="display-1 text--primary">
+                  el·ee·mos·y·nar·y
+                </p>
+                <p>adjective</p>
+                <div class="text--primary">
+                  relating to or dependent on charity; charitable.<br>
+                  "an eleemosynary educational institution."
+                </div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn
+                  text
+                  color="teal accent-4"
+                  @click="reveal = true"
+                >
+                  Learn More
+                </v-btn>
+              </v-card-actions>
+
+              <v-expand-transition>
+                <v-card
+                  v-if="reveal"
+                  class="transition-fast-in-fast-out v-card--reveal"
+                  style="height: 100%;"
+                >
+                  <v-card-text class="pb-0">
+                    <p class="display-1 text--primary">
+                      Origin
+                    </p>
+                    <p>late 16th century (as a noun denoting a place where alms were distributed): from medieval Latin eleemosynarius, from late Latin eleemosyna ‘alms’, from Greek eleēmosunē ‘compassion’ </p>
+                  </v-card-text>
+                  <v-card-actions class="pt-0">
+                    <v-btn
+                      text
+                      color="teal accent-4"
+                      @click="reveal = false"
+                    >
+                      Close
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-expand-transition>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
   </v-app>
