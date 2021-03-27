b1a5931f512a6faf72af8d37213fa8e36f659b99
Uhucream <aborog1324@yahoo.co.jp>
2021-03-27 17:54:45 +0900

    微修正

--- src/components/pages/AboutMe.vue
+++ src/components/pages/AboutMe.vue
@@ -1,7 +1,5 @@
 <template>
-  <v-container
-   fluid
-  >
+  <v-container>
     <v-breadcrumbs :items="crumbsItem" class="pt-1 pl-2">
       <template v-slot:divider>
         <v-icon>mdi-chevron-right</v-icon>
@@ -12,31 +10,29 @@
         class="pa-3"
         height="100%"
       >
-        <v-card-text>
-        </v-card-text>
-          <div class="ml-3 pt-3">
-            <p class="display-1 text--primary">
-              概要
-            </p>
-            <p>
-              <ul class="pb-2 body-1 text--primary">
-                <li>出身地: 北海道札幌市</li>
-                <li>出身高校: 札幌光星中学・高等学校</li>
-                <li>大学: 金沢工業大学</li>
-                <li>趣味: 写真撮影、サイクリング</li>
-              </ul>
-            </p>
-            <p class="display-1 text--primary">
-              自己紹介
-            </p>
-            <div class="body-1 text--primary">
-              <p>生まれ、育ちともに北海道札幌市です。</p>
-              <p>よく、「なぜ首都圏の大学を選ばなかったの？」と聞かれるのですが、富山が好きだったから、というのが端的な理由です。
-              多いときでは一ヶ月に2回も行くほど重度の富山好きです。北陸で情報工学を学べる大学を探していて最終的に金沢工業大学を選びました。</p>
-              <p>中学のときから写真を撮ることが趣味で、最近はたまに気が向いたときに一眼か
-              iPhone 12 Pro Maxで写真を撮ってます。トップページのカルーセルの写真はすべて僕が撮影したものです。</p>
-            </div>
+        <div class="pa-5">
+          <p class="display-1 text--primary">
+            概要
+          </p>
+          <p>
+            <ul class="pb-2 body-1 text--primary">
+              <li>出身地: 北海道札幌市</li>
+              <li>出身高校: 札幌光星中学・高等学校</li>
+              <li>大学: 金沢工業大学</li>
+              <li>趣味: 写真撮影、サイクリング</li>
+            </ul>
+          </p>
+          <p class="display-1 text--primary">
+            自己紹介
+          </p>
+          <div class="body-1 text--primary">
+            <p>生まれ、育ちともに北海道札幌市です。</p>
+            <p>よく、「なぜ首都圏の大学を選ばなかったの？」と聞かれるのですが、富山が好きだったから、というのが端的な理由です。
+            多いときでは一ヶ月に2回も行くほど重度の富山好きです。北陸で情報工学を学べる大学を探していて最終的に金沢工業大学を選びました。</p>
+            <p>中学のときから写真を撮ることが趣味で、最近はたまに気が向いたときに一眼か
+            iPhone 12 Pro Maxで写真を撮ってます。トップページのカルーセルの写真はすべて僕が撮影したものです。</p>
           </div>
+        </div>
       </v-card>
     </div>
   </v-container>
